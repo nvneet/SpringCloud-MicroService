94b2e1448a48bf96078a9a5094fcf79b0cc1b50b
nvneet <navneet.ism@gmail.com>
2023-07-01 16:05:32 +0530

    generating JWT token on successful authentication

--- Impl_2_PhotoAppApiUsers/src/main/java/com/nav/photoapp/api/users/security/AuthenticationFilter.java
+++ Impl_2_PhotoAppApiUsers/src/main/java/com/nav/photoapp/api/users/security/AuthenticationFilter.java
@@ -1,7 +1,13 @@
 package com.nav.photoapp.api.users.security;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,6 +22,8 @@ import com.nav.photoapp.api.users.dto.UserDTO;
 import com.nav.photoapp.api.users.service.IUsersService;
 import com.nav.photoapp.api.users.ui.model.LoginRequestModel;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -34,6 +42,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 	}
 
 	@Override
+	// attemptAuthentication method from UsernamePasswordAuthenticationFilter superclass
 	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
 			throws AuthenticationException {
 		try {
@@ -52,12 +61,34 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 	}
 
 	@Override
+	// successfulAuthentication method from UsernamePasswordAuthenticationFilter <- AbstractAuthenticationProcessingFilter superclass
 	protected void successfulAuthentication(HttpServletRequest req, 
 			HttpServletResponse res, FilterChain chain,
 			Authentication auth) throws IOException, ServletException {
 
 		String userName = ((User)auth.getPrincipal()).getUsername();
 		UserDTO user = iUsersService.getUserDetailsByEmail(userName); // user name is user's e-mail
+
+		Instant now = Instant.now();
+		
+		// reading values from property file
+		String token_expiry = env.getProperty("token.expiration_time");
+		String token_secret = env.getProperty("token.secret");
+		
+		// For signing key for jwt access token
+		byte[] secretKeyBytes = Base64.getEncoder().encode(token_secret.getBytes());
+		SecretKey secretKet = new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS512.getJcaName());
+		
+		// Create JWT token
+		String token = Jwts.builder().setSubject(user.getUserId())
+		.setExpiration(Date.from(now.plusMillis(Long.parseLong(token_expiry))))
+		.setIssuedAt(Date.from(now))
+		.signWith(secretKet, SignatureAlgorithm.HS512)
+		.compact();
+		
+		// now add the created jwt token to HttpServletResponse header and returning back to client app
+		res.addHeader("token", token);
+		res.addHeader("userId", user.getUserId());
 	}
 
 }
